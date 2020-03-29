1acc58ec15ec114df2476ff2f9130fc16a8fe82e
nettoandrade <antonio_netu@hotmail.com>
2020-03-28 22:00:51 -0300

    Obtendo os Contatos

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {Feather} from '@expo/vector-icons';
+import * as Contacts from 'expo-contacts';
+import { Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
 
 import styles from './styles';
+import icon from '../../assets/icon.png';
 
 export default function Incidents() {
+
+  async function findContacts(){
+    
+    const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Name],
+        });
+
+        if (data.length > 0) {
+          const contact = data[0];
+          console.log(data[0].name);
+        }
+      }
+    
+  }
+
   return (
     <View style={styles.container}>
         <View style={styles.header}>
-            <Text>Listar Contatos</Text>
-        </View>
+          <Image source={icon}/>
+        </View>        
+        <Text style={styles.title}>Listar Contatos</Text>
+        <TouchableOpacity style={styles.button} onPress={findContacts}>
+        <Text style={styles.textbutton}>#Listar Contatos IPTV</Text>
+        <Feather name="align-center" size={20} color="#E02041"/>
+        </TouchableOpacity>
+        <FlatList
+        data={[1,2,3,4]}
+        style={styles.contactlist}
+        keyExtractor={ contacts => String(contacts)}
+        renderItem={() => (
+          <View style={styles.contact}>
+            <Text style={styles.textContact}>Name</Text>
+            <Text style={styles.textContact}>Contato</Text>
+          </View>
+        )}
+        />
     </View>
   );
 }
--- src/pages/Incidents/styles.js
+++ src/pages/Incidents/styles.js
@@ -5,11 +5,38 @@ export default StyleSheet.create({
     container: {
         flex: 1,
         paddingHorizontal: 24,
-        paddingTop: Constants.statusBarHeight + 20
+        paddingTop: Constants.statusBarHeight + 20,
+        backgroundColor: '#FFF'
     },
     header: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center'
+    },
+    title:{
+        fontSize: 30,
+        marginBottom: 10,
+        marginTop:30,
+        color: '#13131a'
+    },
+    button: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    textbutton: {
+        color:'#E02041'
+    },
+    contactlist:{
+        marginTop:32
+    },
+    contact:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        color:'#E02041',
+        marginTop: 5
+    },
+    textcontact:{
+        
     }
 });
